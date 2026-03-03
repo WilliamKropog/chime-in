b57da02db5e91087ec1e03c81eebdcce164b7567
WilliamKropog <william.kropog@outlook.com>
2026-03-03 11:32:06 -0600

    Cleaned and finalized the Following Page and its Recommended Posts. Added other QoL changes to Posts and Comments.

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Comment, Post } from 'src/interface';
 import { PostsService } from 'src/services/posts.service';
 
@@ -8,19 +9,28 @@ import { PostsService } from 'src/services/posts.service';
     styleUrl: './comment-list.component.css',
     standalone: false
 })
-export class CommentListComponent implements OnInit{
+export class CommentListComponent implements OnInit {
   @Input() post?: Post;
   @Input() comments: Comment[] = [];
   topComment?: Comment;
-  isCommentSectionOpen: boolean = false;
+  isCommentSectionOpen = false;
 
-  constructor(private postsService: PostsService) {}
+  constructor(
+    private postsService: PostsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    // On the dedicated post page (/post/:postId), start with comments open so the user sees the full discussion
+    const url = this.router.url;
+    const postPageMatch = /^\/post\/([^/?#]+)/.exec(url);
+    if (postPageMatch && postPageMatch[1] === this.post?.postId) {
+      this.isCommentSectionOpen = true;
+    }
     if (this.post?.postId) {
       this.postsService.getTopCommentForPost(this.post.postId).subscribe(topComment => {
         this.topComment = topComment;
-      })
+      });
     }
   }
 
